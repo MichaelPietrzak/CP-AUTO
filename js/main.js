3de09f77b2cf2604f9594f4b5f693164d10d5875
MichalKowalczyk <michal.p.kowalczyk@gmail.com>
2018-04-17 22:41:19 +0200

    Fix responsive, few paddings/margins, typography

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ $('#toggle').click(function() {
    $('.btn-group').on('click', '.btn', function() {
     $(this).addClass('active').siblings().removeClass('active');
   });
+
+  $('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top 
+      }, 1000);
+    }
+  });
