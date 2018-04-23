8685a90eb113d320c8d89ec1411d2289a45d3bc7
MichalKowalczyk <michal.p.kowalczyk@gmail.com>
2018-04-24 00:25:35 +0200

    Fix scroll on click hash

--- js/main.js
+++ js/main.js
@@ -1,32 +1,37 @@
 
-/* Hamburger Menu Option */ 
+/* Hamburger Menu Option */
 
-$('#toggle').click(function() {
-    $(this).toggleClass('active');
-    $('#overlay').toggleClass('open');
-   });
+$('#toggle').click(function () {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
+  $('#overlay').show();
+});
 
-    // Close after clicking on a li
- 
- $('#overlay li').on('click', function(){
+// Close after clicking on a li
+
+$('#overlay li').on('click', function () {
   $("#overlay").hide();
   $("#toggle").removeClass("active");
 });
 
 
 /* ********************************** */
-  
-   $(".btn-group > .btn").click(function(){
-    $(".btn-group > .btn").removeClass("active");
-    $(this).addClass("active");
+
+$(".btn-group > .btn").click(function () {
+  $(".btn-group > .btn").removeClass("active");
+  $(this).addClass("active");
 });
 
-  $('a[href^="#"]').on('click', function (event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top 
-      }, 1000);
-    }
-  });
+$('a[href^="#"]').on('click', function (event) {
+  if(event.originalEvent.path[3].nodeName == 'NAV'){
+    $("#toggle").addClass("active");
+    $('#overlay').toggleClass('open');
+  }
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  }
+});
